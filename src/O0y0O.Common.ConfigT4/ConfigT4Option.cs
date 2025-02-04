1b3f91c0985952eee9f4315150dbaff97c103a21
jason.chung <shiningjason1989@gmail.com>
2025-02-04 12:52:48 +0800

    fix: config option constructor

--- src/O0y0O.Common.ConfigT4/ConfigT4Option.cs
+++ src/O0y0O.Common.ConfigT4/ConfigT4Option.cs
@@ -1,6 +1,6 @@
 namespace O0y0O.Common.ConfigT4;
 
-public class ConfigT4Option(string sectionName, string? sectionKey)
+public class ConfigT4Option(string sectionName, string? sectionKey = default)
 {
     public string SectionName => sectionName;
 
